b02364d2d4e2fe8a218b936d0d41bf383d2b781e
gtwirth <42541127+gtwirth@users.noreply.github.com>
2023-02-15 15:48:44 -0500

    Changed Icon Order...

--- DDMRPCalculatorButtons/CalcButtons.cs
+++ DDMRPCalculatorButtons/CalcButtons.cs
@@ -70,71 +70,6 @@ namespace DDMRPCalculatorButtons
         #endregion
     }
 
-    internal class CalcBuffersButton : IDesignAddIn, IDesignAddInGuiDetails
-    {
-        #region IDesignAddIn Members
-
-        /// <summary>
-        /// Property returning the name of the add-in. This name may contain any characters and is used as the display name for the add-in in the UI.
-        /// </summary>
-        public string Name
-        {
-            get { return "Calc Buffers"; }
-        }
-
-        /// <summary>
-        /// Property returning a short description of what the add-in does.
-        /// </summary>
-        public string Description
-        {
-            get { return "Calculate Buffer Zone Sizes."; }
-        }
-
-        /// <summary>
-        /// Property returning an icon to display for the add-in in the UI.
-        /// </summary>
-        public System.Drawing.Image Icon
-        {
-            get { return Properties.Resources.CalcBuffers; }
-        }
-
-        /// <summary>
-        /// Method called when the add-in is run.
-        /// </summary>
-        public void Execute(IDesignContext context)
-        {
-            // This example code places some new objects from the Standard Library into the active model of the project.
-            if (context.ActiveModel != null)
-            {
-                if (context.ActiveModel != null)
-                {
-                    DDMRPCalculatorButtonsUtils.RunCalculator(context.ActiveModel, "BufferZoneSizes", "Buffer Zone Sizes Calculator");
-                }
-            }
-        }
-
-        #endregion
-
-        #region IDesignAddInGuiDetails Members
-
-        public string CategoryName
-        {
-            get { return "Table Tools"; }
-        }
-
-        public string TabName
-        {
-            get { return "Content"; }
-        }
-
-        public string GroupName
-        {
-            get { return "DDMRP Tools"; }
-        }
-
-        #endregion
-    }
-
     internal class CalcDLTsADUButton : IDesignAddIn, IDesignAddInGuiDetails
     {
         #region IDesignAddIn Members
@@ -174,6 +109,71 @@ namespace DDMRPCalculatorButtons
                 if (context.ActiveModel != null)
                 {
                     DDMRPCalculatorButtonsUtils.RunCalculator(context.ActiveModel, "Inventories", "Decoupled Lead Times Calculator");
+                }
+            }
+        }
+
+        #endregion
+
+        #region IDesignAddInGuiDetails Members
+
+        public string CategoryName
+        {
+            get { return "Table Tools"; }
+        }
+
+        public string TabName
+        {
+            get { return "Content"; }
+        }
+
+        public string GroupName
+        {
+            get { return "DDMRP Tools"; }
+        }
+
+        #endregion
+    }
+
+    internal class CalcBuffersButton : IDesignAddIn, IDesignAddInGuiDetails
+    {
+        #region IDesignAddIn Members
+
+        /// <summary>
+        /// Property returning the name of the add-in. This name may contain any characters and is used as the display name for the add-in in the UI.
+        /// </summary>
+        public string Name
+        {
+            get { return "Calc Buffers"; }
+        }
+
+        /// <summary>
+        /// Property returning a short description of what the add-in does.
+        /// </summary>
+        public string Description
+        {
+            get { return "Calculate Buffer Zone Sizes."; }
+        }
+
+        /// <summary>
+        /// Property returning an icon to display for the add-in in the UI.
+        /// </summary>
+        public System.Drawing.Image Icon
+        {
+            get { return Properties.Resources.CalcBuffers; }
+        }
+
+        /// <summary>
+        /// Method called when the add-in is run.
+        /// </summary>
+        public void Execute(IDesignContext context)
+        {
+            // This example code places some new objects from the Standard Library into the active model of the project.
+            if (context.ActiveModel != null)
+            {
+                if (context.ActiveModel != null)
+                {
+                    DDMRPCalculatorButtonsUtils.RunCalculator(context.ActiveModel, "BufferZoneSizes", "Buffer Zone Sizes Calculator");
                 }
             }
         }
